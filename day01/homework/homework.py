7c718fa5313778a20c8ae6734d63d9687b783642
andria09435 <dali.wkrialashvili88@gail.com>
2024-10-13 18:04:05 +0400

    first commit

---
+++ day01/homework/homework.py
@@ -0,0 +1,36 @@
+name ="andria"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# ="andria" არის ცვლადისთვის მნიშვმელობა
+
+surname = "tsiklauri"
+
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "andria" #ეს არის str (string) ტიპის ცვლადი
+age = 11 # ეს არის int (intejer) მთელი რიცხვი 
+height = 1.60 # ეს არის float ტიპის ცვლადი (ათწილადი) 
+#boolean (bool) ტიპის ცვლადი 
+
+knows_progamming = True  #True ან false
+is_ugly = False  #snakecase( უბრალოდ წერის სტილი სტადარტულად)
+
+isUgli = False # ჯავასკრიპტულ camelcase
+
+
+#print(name + " " +  surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + age)
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_progamming))
+
+print(name + surname + "11" )
+
+print(name + " " + str(age))
+
