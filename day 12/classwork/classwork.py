2392a2e1f29b34a20c6c4b3c1c92e509161963c8
andria09435 <dali.wkrialashvili88@gail.com>
2024-12-23 15:40:14 +0400

    first commit

---
+++ day 12/classwork/classwork.py
@@ -0,0 +1,25 @@
+n1 = 5 > 9
+n2 = 7 < 4
+
+print(n1 and n2) # folse
+
+
+n3 = 9 > 5
+n4 = 8 < 9
+
+print(n3 and n4) # true
+
+n5 = 7 < 9 
+n6 = 7 > 9
+
+print(n5 and n6) # folse
+
+n7 = 9 > 8
+n8 = 7 < 9
+
+print(n7 and n8) # true
+
+n9 = 0 < 1
+n10 = 2 > 9 
+
+print(n9 and n10) # folse
