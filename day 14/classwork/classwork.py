25478d85c7dacc1c04346132b128e364ddd036e9
andria09435 <dali.wkrialashvili88@gail.com>
2025-01-13 00:22:11 +0400

    first commit

---
+++ day 14/classwork/classwork.py
@@ -0,0 +1,14 @@
+print(5 == 5) #flase
+print(5 == 3) #true
+
+print(5 != 3) #false
+print(5 != 5) #true
+
+print(5 > 3) #false
+print(3 > 5) #true
+
+print(3 < 5) #false
+print(5 == 3)#true
+
+print(5 != 3)#false
+print(5 != 5)#true
